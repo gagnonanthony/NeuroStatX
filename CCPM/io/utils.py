989c66f4ea81e61e4f72590c123064672c191d48
gagnonanthony <79757265+gagnonanthony@users.noreply.github.com>
2023-02-23 10:12:56 -0500

    Add load in any format function

---
+++ CCPM/io/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+
+def load_df_in_any_format(file):
+    """
+    Load dataset in any .csv or .xlsx format.
+    :param df:
+    :return:
+    """
+    _, ext = os.path.splitext(file)
+    if ext == '.csv':
+        df = pd.read_csv(file, header=True)
+    if ext == '.xlsx':
+        df = pd.read_excel(file, header=True)
+
+    return df
+
