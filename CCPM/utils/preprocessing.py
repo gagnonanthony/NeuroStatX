600280e929a4deb0a38a1c84e9286b099c72f4e3
gagnonanthony <79757265+gagnonanthony@users.noreply.github.com>
2023-03-10 15:59:04 -0500

    Added shapiro-wilk and correlation coefficient functions

--- CCPM/utils/preprocessing.py
+++ CCPM/utils/preprocessing.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from scipy.stats import shapiro
 
 
 def remove_nans(df):
@@ -17,7 +18,7 @@ def remove_nans(df):
     return rows_with_nans, complete_rows
 
 
-def plot_distributions(df, out_folder, context='poster', font_scale=1, aspect=1.5):
+def plot_distributions(df, out_folder, context='poster', font_scale=1):
     """
     Script to visualize distribution plots for a complete dataframe.
     :param df:
@@ -42,5 +43,48 @@ def plot_distributions(df, out_folder, context='poster', font_scale=1, aspect=1.
         plt.savefig(f'{out_folder}/{var}.png')
 
 
+def compute_shapiro_wilk_test(df):
+    """
+    Function computing the normality statistic using the Shapiro Wilk's test for normality
+    and outputting W and p values.
+    :param df:
+    :return:
+    """
+
+    wilk = []
+    pvalues = []
+
+    for var in tqdm(df.columns):
+        var_data = df[var].values
+        w, pval = shapiro(var_data)
+        wilk.append(w)
+        pvalues.append(pval)
+
+    return wilk, pvalues
 
 
+def compute_correlation_coefficient(df, out_folder, context='poster', font_scale=1, cmap=None,
+                                    annot=False):
+    """
+    Function to compute a correlation matrix for all variables in a dataframe.
+    :param df:
+    :param out_folder:
+    :param context:
+    :param font_scale:
+    :param cmap:
+    :param annot:
+    :return:
+    """
+
+    # Setting plotting parameters.
+    plt.rcParams['figure.figsize'] = [10, 7]
+    plt.rcParams['figure.autolayout'] = True
+    sns.set_style('white')
+    sns.set_context(f'{context}', font_scale)
+
+    corr_mat = df.corr()
+    sns.heatmap(corr_mat, cmap=cmap, annot=annot, square=True, xticklabels=True, yticklabels=True,
+                cbar=True)
+    plt.savefig(f'{out_folder}/correlation_heatmap.png')
+
+    return corr_mat
