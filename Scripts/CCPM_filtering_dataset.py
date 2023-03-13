2d79f9a9f801eaf29a06aa889916b2f4c488bd55
gagnonanthony <79757265+gagnonanthony@users.noreply.github.com>
2023-03-13 17:27:10 -0400

    Adding report option in a pdf

--- Scripts/CCPM_filtering_dataset.py
+++ Scripts/CCPM_filtering_dataset.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+=============================================================================
+            ________    ________   ________     ____     ____
+           /    ____|  /    ____| |   ___  \   |    \___/    |
+          /   /       /   /       |  |__|   |  |             |
+         |   |       |   |        |   _____/   |   |\___/|   |
+          \   \_____  \   \_____  |  |         |   |     |   |
+           \________|  \________| |__|         |___|     |___|
+              Children Cognitive Profile Mapping Toolbox©
+=============================================================================
+
+CCPM_filtering_dataset is designed to compute basic statistics, plotting
+distributions graphs and correlation matrix of raw data.
+
+Initial filtering and evaluating steps are:
+    1)  Removing rows containing NaNs. (2 dataframes will be outputted, one
+        with the cleaned dataset and one with the excluded rows.) Both files
+        should be validated after running this script to ensure correct filtering.
+    2)  Computing global basic statistics. Stats included are : count, mean, std, min,
+        25%, 50%, 75%, max, Wilk and Wilk p-values.
+    3)  Plotting distributions graph. 2 plots are outputted : histogram with kde and
+        ecdf plot.
+    4)  Computing correlation matrix. Correlation between all variables from the dataset
+        is computed and plotted as a heatmap.
+    5)  Final pdf file with indications based on descriptive statistics is outputted (if --report
+        is selected). Indications will highlight variables with W statistic < 0.95 and pair of variables
+        with pearson value > 0.8.
+
+Manual evaluation of all outputted results is recommended to ensure the script behaved
+correctly.
+
+EXAMPLE USAGE :
+CCPM_filtering_dataset.py -i FILE -o OUT_FOLDER -n 1
+
+"""
+
 import argparse
 import logging
+import os
 
+import numpy as np
 import pandas as pd
 
-from CCPM.io.utils import load_df_in_any_format
+from CCPM.io.utils import (load_df_in_any_format,
+                           PDF,
+                           assert_input,
+                           assert_output_dir_exist,
+                           add_verbose_arg,
+                           add_overwrite_arg)
 from CCPM.utils.preprocessing import (remove_nans,
                                       plot_distributions,
                                       compute_shapiro_wilk_test,
@@ -28,7 +71,7 @@ def _build_arg_parser():
     p.add_argument('--disable_plotting', action='store_true',
                    help='If used, will disable all plotting and simply output tables.')
 
-    plot = p.add_argument_group('Plot Options')
+    plot = p.add_argument_group('Matplotlib Options')
     plot.add_argument('--context', choices={'paper', 'poster', 'talk', 'notebook'}, default='poster',
                       help='Context to use as plot style.')
     plot.add_argument('--font_scale', type=int, default=1,
@@ -41,6 +84,17 @@ def _build_arg_parser():
                       help='If true, heatmap will show pearson correlation coefficient in the center \n'
                            'of each square.')
 
+    report = p.add_argument_group('Report Options')
+    report.add_argument('--report', action='store_true', default=False,
+                        help='If used, script will output a report with basic recommendations in a pdf file.')
+    report.add_argument('--wilk_threshold', type=float, default=0.95,
+                        help='Threshold to use for the Wilk value.')
+    report.add_argument('--corr_threshold', type=float, default=0.8,
+                        help='Threshold to use for correlation coefficient.')
+
+    add_verbose_arg(p)
+    add_overwrite_arg(p)
+
     return p
 
 
@@ -48,16 +102,22 @@ def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
+    if args.verbose:
+        logging.getLogger().setLevel(logging.INFO)
+
+    assert_input(parser, args.in_dataset)
+    assert_output_dir_exist(parser, args, args.out_folder, create_dir=True)
+
     raw_df = load_df_in_any_format(args.in_dataset)
     descriptive_columns = [n for n in range(0, args.nb_descriptive_columns)]
 
     # Removing NaNs from dataset and saving the rows in a different file.
-    logging.debug('Filtering dataset to remove NaN values. ')
+    logging.info('Filtering dataset to remove NaN values. ')
     nans, clean = remove_nans(raw_df)
 
     # Exporting global description statistics.
     variable_for_stats = clean.drop(clean.columns[descriptive_columns], axis=1, inplace=False)
-    logging.debug('Computing global descriptive statistics.')
+    logging.info('Computing global descriptive statistics.')
     description_df = variable_for_stats.describe()
     wilk, pvalues = compute_shapiro_wilk_test(variable_for_stats)
     w_pval = pd.DataFrame([wilk, pvalues], index=['Wilk', 'Wilk pvalues'], columns=description_df.columns)
@@ -66,21 +126,53 @@ def main():
 
     # Plotting functions and/or computing correlation heatmap.
     if args.disable_plotting:
-        logging.debug('Plotting is disabled, skipping the plotting steps.')
+        logging.info('Plotting is disabled, skipping the plotting steps.')
         corr_mat = variable_for_stats.corr()
 
     else:
-        logging.debug('Plotting distributions for the complete dataframe.')
+        logging.info('Plotting distributions for the complete dataframe.')
+        path_plot = os.path.join(args.out_folder + '/' + 'Distribution_Plots')
+        os.makedirs(path_plot, exist_ok=True)
         variable_to_plot = clean.drop(clean.columns[descriptive_columns], axis=1, inplace=False)
         # Plotting distributions.
-        plot_distributions(variable_to_plot, args.out_folder, context=args.context, font_scale=args.font_scale)
+        plot_distributions(variable_to_plot, path_plot, context=args.context, font_scale=args.font_scale)
 
         # Plotting heatmap and computing correlation matrix.
-        logging.debug('Generating correlation matrix and heatmap.')
+        logging.info('Generating correlation matrix and heatmap.')
         corr_mat = compute_correlation_coefficient(variable_to_plot, args.out_folder, context=args.context,
                                                    font_scale=args.font_scale, cmap=args.cmap, annot=args.annotate)
 
     corr_mat.to_excel(f'{args.out_folder}/correlation_coefficient.xlsx', header=True, index=True)
+
+    # Generating basic recommendations.
+    if args.report:
+        logging.info('Generating the report...')
+        pdf = PDF()
+        pdf.alias_nb_pages()
+        pdf.set_font('Times', '', 12)
+
+        wilk_value = description_df.loc['Wilk',]
+        with open('basic_stats.txt', 'w') as f:
+            for i in range(0, len(wilk_value)):
+                if wilk_value[i] < args.wilk_threshold:
+                    f.write(f"Variable {wilk_value.index[i]} present a W < 0.95 ({round(wilk_value[i], 2)}), you may "
+                            f"need to inspect the histogram and ecdf plots to determine the suitability "
+                            f"for parametric test. \n")
+
+        pdf.print_chapter(1, 'Normality concerns : ', 'basic_stats.txt')
+
+        with open('correlation_stats.txt', 'w') as f:
+            corr = np.triu(corr_mat.to_numpy(), 1)
+            for i in range(0, corr.shape[0]):
+                for j in range(0, corr.shape[1]):
+                    if corr[i, j] > args.corr_threshold:
+                        f.write(f"Variables {description_df.columns[i]} and {description_df.columns[j]} "
+                                f"present a high pearson correlation coefficient > 0.8 ({round(corr[i, j], 3)}). It may"
+                                f" be interesting to evaluate the need of keeping both variables since they carry "
+                                f"similar information. \n")
+
+        pdf.print_chapter(2, 'Correlation concerns : ', 'correlation_stats.txt')
+        pdf.output(f'{args.out_folder}/report.pdf', 'F')
 
 
 if __name__ == '__main__':
