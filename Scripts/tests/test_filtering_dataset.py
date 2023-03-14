5a1525e6b2f8d89a7799c60941539eec5a3d066a
gagnonanthony <79757265+gagnonanthony@users.noreply.github.com>
2023-03-14 13:25:12 -0400

    New test file

--- Scripts/tests/test_filtering_dataset.py
+++ Scripts/tests/test_filtering_dataset.py
@@ -4,15 +4,25 @@
 import os
 import tempfile
 
-from CCPM.io.download import get_home, download_data, download_file_from_GDrive
+from CCPM.io.download import get_home, download_data, save_files_dict
 
 
-#download_data(download_file_from_GDrive(), keys=['data.zip'])
-tmp_dir = tempfile.TemporaryFile()
+download_data(save_files_dict(), keys=['data.zip'])
+tmp_dir = tempfile.TemporaryDirectory()
 
 
 def test_help(script_runner):
     ret = script_runner.run('CCPM_filtering_dataset.py',
                             '--help')
+
+    assert ret.success
+
+
+def test_execution_filtering(script_runner):
+    os.chdir(os.path.expanduser(tmp_dir.name))
+    in_dataset = os.path.join(get_home(), 'data/data_example.xlsx')
+    out_folder = os.path.join(get_home(), 'data/Filtering/')
+    ret = script_runner.run('CCPM_filtering_dataset.py', '-i', in_dataset, '-o', out_folder, '-n', '1',
+                            '--report', '-f')
 
     assert ret.success
