402b06fa0faa39fd77e652421c5a059a8872f665
Anthony Gagnon <anthony.gagnon7@usherbrooke.ca>
2024-08-02 15:49:50 -0400

    fix sed, path to root astro project

--- docs/organize_files.sh
+++ docs/organize_files.sh
@@ -22,13 +22,13 @@ for file in neurostatx.*.*.md; do
   folder_name=$(echo "$middle_part")
 
   # Modify the file content to convert the specified lines
-  sed -i '' -E "s/(neurostatx\.$middle_part\.)$third_part\.(.*)/ \2/" "$file"
+  sed -i -E "s/(neurostatx\.$middle_part\.)$third_part\.(.*)/ \2/" "$file"
   
   # Replace all # with ####
-  sed -i '' 's/^# /#### /' "$file"
+  sed -i 's/^# /#### /' "$file"
 
   # Replace title in the frontmatter
-  sed -i '' "s/title: \"neurostatx\.$middle_part\.$third_part\"/title: \"$third_part\"/" "$file"  
+  sed -i "s/title: \"neurostatx\.$middle_part\.$third_part\"/title: \"$third_part\"/" "$file"  
 
   # Create the folder within the output directory if it doesn't exist
   mkdir -p "$output_dir/$folder_name"
